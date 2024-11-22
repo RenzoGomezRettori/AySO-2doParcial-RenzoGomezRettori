723b1515de820b27dbf2e153310e2638e2cee268
RenzoGomezRettori <renzogomezrettori93@gmail.com>
2024-11-22 01:05:04 +0000

    Finalización del punto b

---
+++ puntob.sh
@@ -0,0 +1,40 @@
+  149  ssh-keygen
+  150  
+  151  ll .ssh/
+  152  cd ..
+  153  ll
+  154  ll .ssh/
+  158  cat .ssh/id_rsa.pub
+  160  ssh vagrant@192.168.56.9
+  161  git clone https://github.com/upszot/UTN-FRA_SO_Ansible.git
+  162  ll
+  163  cd UTN-FRA_SO_Ansible/
+  164  ll
+  165  cd ejemplo_02/
+  166  ll
+  167  vim inventory
+  168  cat vim inventory
+  169  vim playbook.yml
+  170  cat vim playbook.yml
+  171  ll
+  172  vim playbook.yml
+  173  ansible-playbook -i inventory playbook.yml
+  174  cd ..
+  ---------------------------------------------------
+  historial de la vm donde se hizo un Update y se instalo Apache
+
+    296  ip address show
+  297  ll .ssh/
+  298  echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCfEMtmy2NNKgvGpltV0L6JD+5qBczaL/fzkTjF835IDdYUImMCnvWxZu4AR0W7tSUf4+1J2BUA3OwfnoZTwPwltae6T4/ptPxNczjIWuD3ZNCMzaJpwXgaikXQN/2dt2VY7B4/hisZy/to2naYilJfGcfgo7FbfFSPxVtR2hjDDNpGcZkpJKH4aRc1VeWcIK7HCGACaCAvE+LIsX2ttc0XxsoOXEhJIr6+rn9slBFpSHxWxfmSgxgqiM3kSmox/Dr+OQmHGV9sB6F9/pAlA7nhkeU7uUS4zS7wZN/I33B6bJP9sB3z5XcDwVRe9RgNcApzo7MqM1ZXNRlXs+od7N6aETAi8fVAGugfs49VdI6gn3qRY/8uj5Ga2SYzMPFpSkQGSG6VIToljYYdquMgHaoq5TeP/G2hkEsEPZQK6idHwj+36bMFYMc4WNAJpjZvC9zlAoiMlVnEFW4zcqY8EmTW+/dh60LLVhYmFFRLE6rdpD1hNt39GB9SYwXWHi/QPd8= vagrant@vm2doParcial" >> .ssh/authorized_keys
+  299  cat vim authorized_keys
+  300  ll
+  301  cd .ssh/
+  302  ll
+  303  cat vim authorized_keys
+  304  cd ..
+  305  ll
+  306  ip
+  307  ip address show
+  308  sudo apt list --installed | grep apache
+  180  history
+
